5e37a8bbc5f62972d616968acb7afad4d44b5fb0
Raul <id00692943@usal.es>
2019-04-10 11:13:15 +0400

    Add musicmap

---
+++ musicmap/precache-manifest.563726f22cc44a26e0b280db6ab29215.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4ff187bbcb0802617826",
+    "url": "/js/chunk-vendors.6aa582c9.js"
+  },
+  {
+    "revision": "7eefbbd33204e95ede21",
+    "url": "/js/app.65eeec1f.js"
+  },
+  {
+    "revision": "4649c7edcce286cf7b772b6e38844673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65218e603e0501d2a316b49f9587a96f",
+    "url": "/img/user.65218e60.svg"
+  },
+  {
+    "revision": "17dfdfdbaf328ee1dfda6afddee162b9",
+    "url": "/img/pinPurple.17dfdfdb.svg"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.8a9a261c.woff2"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.c38ebd3c.woff"
+  },
+  {
+    "revision": "4ff187bbcb0802617826",
+    "url": "/css/chunk-vendors.2ecdf0e7.css"
+  },
+  {
+    "revision": "7eefbbd33204e95ede21",
+    "url": "/css/app.14634b35.css"
+  }
+];
